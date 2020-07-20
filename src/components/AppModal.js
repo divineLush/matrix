42256e54fa3b9843602c3651e864af5dda615a5f
divineLush <homeoflucifer@gmail.com>
2020-07-20 16:04:58 +0700

    improved modal matrix rendering

--- src/components/AppModal.js
+++ src/components/AppModal.js
@@ -8,18 +8,50 @@ const AppModal = (props) => {
       .map(num => calcMatrixElCoords(props.matrix, num))
       .sort((a, b) => a[0] - b[0])
 
+    const matrixWidth = (() => {
+      const areEqual = (() => {
+        const arr = elements.map(el => el[1])
+
+        return arr.every(el => el === arr[0])
+      })()
+
+      const calcWidth = () => {
+        let max = -1
+        let min = 6
+
+        for (let i = 0; i < elements.length; i++) {
+          const el = elements[i][1]
+          if (el >= max)
+            max = el
+
+          if (el <= min)
+            min = el
+        }
+
+        return Math.abs(max - min) + 1
+      }
+
+      return areEqual ? 1 : calcWidth()
+    })()
+
     const checkboxVisibilities = elements
       .map(el => props.matrix[el[0]][el[1]] === 1 ? true : false)
 
     const renderGrid = () =>
       elements
         .map((el, key) =>
-          <input
-            key={ key }
-            type="checkbox"
-            checked={ checkboxVisibilities[key] }
-            onChange={ () => props.checkboxHandler(el) }
-          />)
+          <div key={ key } style={{ textAlign: 'center' }}>
+            <input
+              type="checkbox"
+              checked={ checkboxVisibilities[key] }
+              onChange={ () => props.checkboxHandler(el) }
+            />
+          </div>)
+
+    const matrixStyle = {
+      display: 'grid',
+      gridTemplateColumns: `repeat(${ matrixWidth }, 1fr)`
+    }
 
     return (
         <Modal show={ props.show }>
@@ -27,7 +59,7 @@ const AppModal = (props) => {
             <Modal.Title>Edit matrix</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <div>
+            <div style={ matrixStyle }>
               { renderGrid() }
             </div>
           </Modal.Body>
